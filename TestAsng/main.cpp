6536193d9b20cd12320afa401d9b48427fce4072
thilina27 <madhusanka.thilina@gmail.com>
2016-04-27 20:19:12 +0530

    Add login for customers

--- TestAsng/main.cpp
+++ TestAsng/main.cpp
@@ -1,42 +1,64 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-const int maxReq;
+const int maxReq=10;
+const int numOfCustomers=2;
+const int numOfTutors=2;
 
 int mainMenu();
 void checkvalid(bool &flag,int mini, int maxi, int choice);
+void setValue(struct Tutor tutors[], struct Customer customers[]);
+bool loginMenu(string &username , string &password);
+bool login(string username , string password, int loginType, struct Tutor tutors[], struct Customer customers[]);
+
+
+//stuctures
+//create structure for tutors
+struct Tutor
+{
+    int id;
+    string name;
+    int workingDays;
+    float hourlyPayment;
+    int experties;
+    string email;
+    string password;
+};
+
+//create structure for customer
+struct Customer
+{
+    int id;
+    string name;
+    string password;
+};
+
+//structure for customer requests
+struct Request
+{
+    int custID;
+    int day;
+    int lession;
+    float maxRate;
+};
 
 int main()
 {
-    //create structure for tutors
-    struct Tutor
-    {
-        int id;
-        char name[30];
-        int workingDays;
-        float hourlyPayment;
-        int experties;
-        string email;
-        char password[20];
-    };
 
-    //structure for customer requests
-    struct Request
-    {
-        int day;
-        int lession;
-        float maxRate;
-    };
+
     //array to store tutors
-    Tutor tutors[10];
+    Tutor tutors[numOfTutors];
 
     //Customer array
+    Customer customers[numOfCustomers];
 
     //request array
+    Request requests[maxReq];
 
     //request array num elements
-    int reqElemnts;
+    int reqElemnts = 0;
 
     //Admin array
 
@@ -46,6 +68,40 @@ int main()
     //logged in or not
     bool isLogged = false;
 
+    //exit
+    bool exit=false;
+
+    setValue(tutors,customers);
+
+    while(!exit){
+
+        //get login type
+        loginType = mainMenu();
+
+        if(loginType == 4){
+            exit = true;
+        }
+        else{
+            //username pasword holder
+            string username;
+            string password;
+            bool back=false;
+
+            while(!isLogged && !back){
+                //show login menu
+                back=loginMenu(username,password);
+                //loged in user
+                if(!back){
+                    isLogged=login(username,password,loginType,tutors,customers);
+                }
+
+            }
+
+        }
+
+
+
+    }
 
     //int a = 4;
     //int b = 7;
@@ -66,7 +122,7 @@ int mainMenu(){
 
     while(!valid){
 
-        cout << " ABC BLA BLA"<<endl;
+        cout << " ABC Company"<<endl;
         cout << "1. Admin"<<endl;
         cout << "2. Customer"<<endl;
         cout << "3. Tutor"<<endl;
@@ -82,14 +138,75 @@ int mainMenu(){
 }
 
 //login menu
-void login(char username[] , char password[]){
-
+bool loginMenu(string &username , string &password){
+    cin.get();
+    char c='e';
     cout << " Login "<<endl;
     cout << "User Name  : ";
-    cin.getline(username,sizeof(username));
+    cin  >>username;
     cout << "Password   : ";
-    cin.getline(password,sizeof(password));
+    cin  >>password;
+    cout <<"enter e to exit or s to submit : ";
+    cin  >>c;
+    if(c == 's'){
+        return false;
+    }
+    else {
+        return true;
+    }
 
+}
+
+//loggin function
+bool login(string username, string password, int loginType, struct Tutor tutors[], struct Customer customers[]){
+
+    cout<<username<<endl;
+    cout<<password<<endl;
+    //log user based on loggin type
+    if(loginType == 1){
+
+        //to do admin
+    }
+    else if(loginType == 2){
+        //customers
+        for(int i=0; i<numOfCustomers; i++) {
+            string a=customers[i].name;
+            cout<<a<<endl;
+            if(a==username){
+                string b=customers[i].password;
+                cout<<b<<endl;
+                if(b==password){
+                    cout << "Login Success"<<endl;
+                    cout << "Rederecting......"<<endl;
+                    return true;
+                }
+
+            }
+
+        }
+        cout << "Loggin failed. Try agin! ";
+        return false;
+
+    }//end customer type log
+    else{
+        //customers
+        for(int i=0; i<numOfTutors; i++) {
+
+            if(tutors[i].name == username){
+
+                if(tutors[i].password == password){
+                    cout << "Login Success"<<endl;
+                    cout << "Rederecting......"<<endl;
+                    return true;
+                }
+
+            }
+
+        }
+        cout << "Loggin failed. Try agin! ";
+        return false;
+
+    }//end tutor type log
 }
 
 //customer menu
@@ -123,9 +240,9 @@ void requestTutorMenu(int prvSelection, struct Request requests[], int &reqElemn
     int lession;
     float maxRate;
 
-    //add values to request array
+
     if(reqElemnts == maxReq){
-        cout << "Cannot make request contact Administrator "
+        cout << "Cannot make request contact Administrator ";
     }
     else{
         cout <<"Enter day of the week (1 for mon ,2 for tues etc) : ";
@@ -135,6 +252,7 @@ void requestTutorMenu(int prvSelection, struct Request requests[], int &reqElemn
         cout <<"Max hourly rate willing to play : ";
         cin  >>maxRate;
 
+        //add values to request array
         reqElemnts++;
         requests[reqElemnts].day = day;
         requests[reqElemnts].lession = lession;
@@ -154,4 +272,36 @@ void checkvalid(bool &flag,int mini, int maxi, int choice){
         cout <<endl<<"PLZ enter valid input "<<endl<<endl;
     }
 
+}
+
+//set values for arrays
+void setValue(struct Tutor tutors[], struct Customer customers[]){
+
+        int i=0;
+
+        tutors[i].id = i;
+        tutors[i].name = "Sam";
+        tutors[i].workingDays = 7;
+        tutors[i].hourlyPayment = 100.00;
+        tutors[i].experties = 7;
+        tutors[i].email = "sjnsjs.ccm";
+        tutors[i].password = "1234";
+        i++;
+        tutors[i].id = i;
+        tutors[i].name = "Jam";
+        tutors[i].workingDays = 8;
+        tutors[i].hourlyPayment = 170.00;
+        tutors[i].experties = 4;
+        tutors[i].email = "sjnsjs.ccm";
+        tutors[i].password = "5678";
+
+        i=0;
+        customers[i].id=i;
+        customers[i].name="trc";
+        customers[i].password = "7897";
+
+        i++;
+        customers[i].id=i;
+        customers[i].name="trc2";
+        customers[i].password = "7891";
 }
